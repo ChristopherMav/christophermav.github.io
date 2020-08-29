d94818e9e6e49f3dd7fe549bea07b7ddc4a9c8a5
BuildTools <unconfigured@null.spigotmc.org>
2020-08-29 17:50:17 +0200

    attempt #1

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 var url = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name=Berlin%202019%20Inferno%20Souvenir%20Package"
-$("button").click(function() 
-{
     $.getJSON(url, function(data)
 		    {
         $("ul").append("<li>"+data.success+"</li>");
@@ -8,4 +6,3 @@ $("button").click(function()
     	$("ul").append("<li>"+data.volume+"</li>");
         $("ul").append("<li>"+data.median_price+"</li>");
     });
-});
